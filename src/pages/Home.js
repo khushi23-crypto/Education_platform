92b489c48f19a77bdf323de3a271d45778d63094
Kajal Saini <sizukabts91@gmail.com>
2025-12-05 19:08:04 +0530

    create home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+
+
+import React from 'react';
+import HeroSection from '../components/Home/HeroSection';
+import Features from '../components/Home/Features';
+import Stats from '../components/Home/Stats';
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <Features />
+      <Stats />
+    </>
+  );
+};
+
+export default Home;
