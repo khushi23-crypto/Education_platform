c1762a6e7321df414157d6c807bbe9691f1acbde
Kajal Saini <sizukabts91@gmail.com>
2025-12-05 19:48:46 +0530

    create enrollment.js

---
+++ knowledge-brewers-backend/models/Enrollment.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const enrollmentSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
+  progress: { type: Number, default: 0 }, // 0-100%
+  enrolledAt: { type: Date, default: Date.now },
+  completedAt: Date
+});
+
+module.exports = mongoose.model('Enrollment', enrollmentSchema);
